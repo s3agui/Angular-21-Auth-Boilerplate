b80c02a503287e42f67dce7bdeff43fdc07451ef
S3agui <cjcuerda.learns@gmail.com>
2026-05-04 12:56:14 +0800

    account modules inserted

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { AccountRoutingModule } from "./account-routing.module";
+import { LayoutComponent } from "@app/_components/layout.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { VerifyEmailComponent } from "./verify-email/verify-email.component";
+import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { ResetPasswordComponent } from "./reset-password/reset-password.component";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AccountRoutingModule
+    ],
+    declarations: [
+        LayoutComponent,
+        LoginComponent,
+        RegisterComponent,
+        VerifyEmailComponent,
+        ForgotPasswordComponent,
+        ResetPasswordComponent
+    ]
+})
+export class AccountModule { }
---
+++ src/app/account/layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AccountService } from '@app/_services';
+
+@component ({ selector: 'account-layout', templateUrl: 'layout.component.html', standalone: false })
+export class LayoutComponent {
+    constructor(
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        if (this.accountService.accountValue) {
+            this.router.navigate(['/']);
+        }
+
+     }
+}
